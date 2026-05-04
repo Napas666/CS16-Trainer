6d976bf1ee7c0519d239e779cdc8b8d1f8255ffe
Napasv2 <Artemiy0100@mail.ru>
2026-05-04 23:16:28 +0300

    fix: configure pip globally before install to fix SSL

--- START.bat
+++ START.bat
@@ -1,13 +1,16 @@
 @echo off
 cd /d "%~dp0"
 
-echo [1/2] Installing dependencies...
-set PIP_TRUSTED_HOST=pypi.tuna.tsinghua.edu.cn
-pip install -r requirements.txt -i http://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+echo [1/3] Configuring pip...
+pip config set global.index-url http://pypi.tuna.tsinghua.edu.cn/simple
+pip config set global.trusted-host pypi.tuna.tsinghua.edu.cn
+
+echo [2/3] Installing dependencies...
+pip install pymem customtkinter pygame pywin32
 
 echo.
-echo [2/2] Starting trainer...
-echo Run CS 1.6 first, then click ATTACH in the app.
+echo [3/3] Starting trainer...
+echo Run CS 1.6 first, then click ATTACH.
 echo.
 python trainer.py
 pause
