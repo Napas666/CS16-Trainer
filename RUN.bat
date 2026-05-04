064dadb14120ba9e5094bef6472998adcd7e60b2
Napasv2 <Artemiy0100@mail.ru>
2026-05-04 22:57:25 +0300

    fix: upgrade pip first + trusted-host for SSL issues

--- RUN.bat
+++ RUN.bat
@@ -1,8 +1,11 @@
 @echo off
 cd /d "%~dp0"
 
-echo Installing dependencies (via mirror)...
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+echo Updating pip...
+python -m pip install --upgrade pip --trusted-host pypi.org --trusted-host files.pythonhosted.org
+
+echo Installing dependencies...
+pip install -r requirements.txt --trusted-host pypi.org --trusted-host files.pythonhosted.org
 
 echo.
 echo Starting CS 1.6 Trainer...
